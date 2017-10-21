4fb36c851833ef2621e6a92195a007135e8e6c88
tdimnet <thomas.dimnet@gmail.com>
2017-10-21 20:27:59 +0200

    Set up the routing systems

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
-var users = require('./routes/users');
+var books = require('./routes/books');
+var patrons = require('./routes/patrons');
+var loans = require('./routes/loans');
+
 
 var app = express();
 
@@ -22,8 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/books', books);
+app.use('/patrons', patrons);
+app.use('/loans', loans);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
