e56787e3c899b2e8b38f61ae0ddccc49c4bbee37
tdimnet <thomas.dimnet@gmail.com>
2017-10-22 13:44:14 +0200

    Create a new template file, implement css and start working with the database

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,17 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
+var Book = require('../models').Book;
+var Patron = require('../models').Patron;
+var Loan = require('../models').Loan;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Books' });
+  res.render('books', {
+    title: 'Books',
+  });
 });
 
 module.exports = router;
