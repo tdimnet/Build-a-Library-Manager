ff421d55d697d115ace2672cbd4f4419c8240c94
tdimnet <thomas.dimnet@gmail.com>
2017-10-23 22:34:11 +0200

    Finish the Patron model

--- models/patron.js
+++ models/patron.js
@@ -5,16 +5,59 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.INTEGER,
       primaryKey: true
     },
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    address: DataTypes.STRING,
-    email: DataTypes.STRING,
-    library_id: DataTypes.STRING,
-    zip_code: DataTypes.INTEGER
+    first_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'First Name has to be filed in'
+        }
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Last Name has to be filed in'
+        }
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Address field has to be filed in'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Email has to be filed in'
+        }
+      }
+    },
+    library_id: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Library ID has to be filed in'
+        }
+      }
+    },
+    zip_code: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notEmpty: {
+          msg: 'Zip code has to be filed in'
+        }
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        this.hasMany(models.Loan, { foreignKey: 'patron_id' })
       }
     },
     timestamps: false
