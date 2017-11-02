7123697f41e28d3413f50e0499b8b89966cd2c22
tdimnet <thomas.dimnet@gmail.com>
2017-11-02 15:20:04 +0100

    Create many to many relation and connect select

--- routes/loans.js
+++ routes/loans.js
@@ -5,9 +5,8 @@ var Book = require('../models').Book;
 var Patron = require('../models').Patron;
 var Loan = require('../models').Loan;
 
-// GET LOANS home page.
+// ROUTE FOR LOANS VIEW
 router.get('/', function(req, res, next) {
-
   var booksOverdue = {
     include: [Book, Patron]
   }
@@ -15,7 +14,6 @@ router.get('/', function(req, res, next) {
   Loan
     .findAndCountAll(booksOverdue)
     .then(function(results) {
-      console.log(results);
     res.render('loan/loans', {
       title: 'Loan',
       loans: results.rows
@@ -24,9 +22,32 @@ router.get('/', function(req, res, next) {
 });
 
 
-// NEW LOAN VIEW
+// ROUTE FOR NEW LOAN
+  // GET
 router.get('/new', function(req, res, next) {
-  res.render('loan/new-loan');
+
+  var loan = Loan.build({
+    loaned_on: '2018-10-10',
+    return_by: '2019-10-20'
+  });
+
+  Book
+    .findAll()
+    .then(function(books) {
+      Patron
+        .findAll()
+        .then(function(patrons) {
+          res.render('loan/new-loan', {
+            title: 'Create a new loan',
+            loan: loan,
+            books: books,
+            patrons: patrons
+          });
+        })
+    })
 });
+
+  // POST
+
 
 module.exports = router;
